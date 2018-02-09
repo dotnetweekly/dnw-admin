af74e34c7f450b9ff494e4657f57e1af345d4efe
bstavroulakis <bstavroulakis@gmail.com>
2018-02-09 00:21:35 -0800

    add newsletter save

--- src/pages/Emails/NewsletterCurrent.vue
+++ src/pages/Emails/NewsletterCurrent.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="has-text-centered">
+    <div style="width:800px;display: inline-block;text-align: left;">
+      <button v-on:click="save()" class="button is-primary is-pulled-right">Generate!</button>
+    </div>
+    <div class="is-clearfix"></div>
     <a v-on:click="changeSize('desktop')">Desktop Size</a>
     <a v-on:click="changeSize('tablet')">Tablet Size</a>
     <a v-on:click="changeSize('mobile')">Mobile Size</a>
@@ -12,6 +16,16 @@ import request from "../../request";
 
 export default {
   methods: {
+    save() {
+      request
+      .post("/emails/newsletter/current", {save: true})
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    },
     changeSize(type) {
       const newsletterIframe = this.$el.querySelector("#newsletter-iframe");
       if(type == "tablet") {
@@ -27,7 +41,7 @@ export default {
   },
   mounted() {
     request
-      .get("/emails/newsletter/current")
+      .post("/emails/newsletter/current")
       .then(response => {
         const newsletterIframe = this.$el.querySelector("#newsletter-iframe");
         newsletterIframe.contentWindow.document.write(response.data.data);
